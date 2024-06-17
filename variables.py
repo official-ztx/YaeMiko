505f32eb69e89e7bf7d20d9fce283bcd8b8ededb
official-ztx <wtuhin707@gmail.com>
2024-06-17 11:52:26 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -10,31 +10,31 @@ class Config(object):
 
     # <================================================ REQUIRED ======================================================>
     # Telegram API configuration
-    API_ID = 6433468  # Get this value from my.telegram.org/apps
-    API_HASH = "7895dfd061f656367ccab30032"
+    API_ID = 23028479  # Get this value from my.telegram.org/apps
+    API_HASH = "c1e6a93b04c0810a5c282d8d8d44ea6f"
 
     # Database configuration (PostgreSQL)
     DATABASE_URL = "postgres://ierjlkr:OG4dxzO67Zret3Zii43Hhvujkg89WVry0n9KsHE@karma.db.elephantsql.com/ierjlkr"
 
     # Event logs chat ID and message dump chat ID
-    EVENT_LOGS = -1001629811868
-    MESSAGE_DUMP = -1001629811868
+    EVENT_LOGS = -1002123758721
+    MESSAGE_DUMP = -1002123758721
 
     # MongoDB configuration
     MONGO_DB_URI = "mongodb+srv://t45:t45@cluster0.plfylpo.mongodb.net/?retryWrites=true&w=majority"
 
     # Support chat and support ID
-    SUPPORT_CHAT = "ProjectCodeXSupport"
-    SUPPORT_ID = -1001629811868
+    SUPPORT_CHAT = "ZTX_BOTS"
+    SUPPORT_ID = -1002123758721
 
     # Database name
-    DB_NAME = "MikoDB"
+    DB_NAME = "UtopiaDB"
 
     # Bot token
     TOKEN = "2323839365:AAFgfdadqawlfdsM7slOa33eM_ghop"  # Get bot token from @BotFather on Telegram
 
     # Owner's Telegram user ID (Must be an integer)
-    OWNER_ID = 5907205317
+    OWNER_ID = 7114763692
     # <=======================================================================================================>
 
     # <================================================ OPTIONAL ======================================================>
